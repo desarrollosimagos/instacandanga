3df1784ac910e80697d5fa3c43b898a08d7a6985
omar <omar@debian>
2017-04-02 14:58:48 -0400

    error de home

--- app/ActivationRepository.php
+++ app/ActivationRepository.php
@@ -19,7 +19,7 @@ class ActivationRepository
 
     protected function getToken()
     {
-		$an = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ-)(.:,;";
+		$an = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     	$su = strlen($an) - 1;
     	return substr($an, rand(0, $su), 1) .
             substr($an, rand(0, $su), 1) .
