8613b23a7bc81ced5b16d5323749aebf72a1b35b
omar <omar@debian>
2017-04-02 02:00:21 -0400

    no

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,6 +23,25 @@
     </script>
 </head>
 <body>
+    <script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1924808381083207',
+      xfbml      : true,
+      version    : 'v2.9'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+
     <div id="app" style="background-color:#d22052">
         <nav class="navbar navbar-default navbar-static-top" style="background-color:#fff;margin-bottom: 0px;">
             <div class="container">
