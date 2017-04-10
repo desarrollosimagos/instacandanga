4aac39d7a4547d9b96200bf8e02e0f291b11ab1c
Omar Orozco <ing.omar.orozco@gmail.com>
2017-04-10 01:26:54 -0400

    notificaciones

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,6 +6,7 @@ use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Http\Request;
 use App\ActivationService;
