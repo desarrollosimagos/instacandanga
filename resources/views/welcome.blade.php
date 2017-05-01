13628a38c90121d20dee7585e0e55659a9334267
Desarrollos Imagos <desarrollosimago@gmail.com>
2017-05-01 08:36:44 -0700

    no

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,12 @@
 <div class="container" style="margin-top:-22px;width:100%;margin-left:-15px;">
     <div class="row">
         <div class="col-md-6 text-left hidden-xs hidden-sm" style="clip:rect(20px 205px 110px 55px);">
-        <a href="{{ url('/registrar') }}">
+        <a href="{{ url('/register') }}">
         <img src="{{ url('img/izq.png') }}" style="margin-left:-110px; ">
         </a>
         </div>
         <div class="col-md-6 text-right">
-        <a href="{{ url('/registrar') }}">
+        <a href="{{ url('/register') }}">
         <img src="{{ url('img/der.png') }}" style="margin-right:-90px;">
         </a>
         </div>
